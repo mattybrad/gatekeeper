fea34704479445af3b058c89ac87f108562391cd
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2018-05-09 13:26:58 +0100

    added potentiometer class but i'll sort it out later

---
+++ src/components/Potentiometer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rawSliderValue: 0,
+      calculatedSliderValue: this.calculateSliderValue(0)
+    }
+  }
+
+  calculateSliderValue(raw) {
+    return this.props.min + (this.props.max-this.props.min) * raw / 10000
+  }
+
+  handleSliderChange() {
+    var raw = this.refs.sliderInput.value;
+    var calculated = this.calculateSliderValue(this.refs.sliderInput.value);
+    this.props.onChange(this.props.label, calculated);
+    this.setState({
+      rawSliderValue: raw,
+      calculatedSliderValue: calculated
+    })
+  }
+
+  componentDidMount() {
+    this.handleSliderChange();
+  }
+
+  render() {
+    return (
+      <div className='slider'>
+        <label>{this.props.label}</label>
+        <input
+          ref='sliderInput'
+          type="range"
+          min={0}
+          max={10000}
+          defaultValue={5000}
+          value={this.state.sliderValue}
+          onMouseMove={this.handleSliderChange.bind(this)}
+          onChange={this.handleSliderChange.bind(this)}
+          />
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+  label: 'slider',
+  min: 0,
+  max: 1,
+  start: 0,
+  onChange: function(){}
+};
+
+export default AppComponent;
